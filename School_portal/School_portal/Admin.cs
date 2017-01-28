05a7f5c901c4605b244620361cba172ed90b6b44
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-28 22:27:05 +0300

    Add class "Admin"

---
+++ School_portal/School_portal/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_portal
+{
+    public class Admin : Users
+    {
+        public int admin_id { get; set; }
+        public Admin(int user_id)
+        {
+            this.user_id = user_id;
+        }
+    }
+}
