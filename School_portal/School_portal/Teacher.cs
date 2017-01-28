91788173d046ba84146ee680ebc4c02f9c686504
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-28 22:28:51 +0300

    Add class "Teacher"

---
+++ School_portal/School_portal/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_portal
+{
+    public class Teacher : Users
+    {
+        public int teacher_id { get; set; }
+        public string position { get; set; }
+        public Teacher(int user_id)
+        {
+            this.user_id = user_id;
+        }
+    }
+}
