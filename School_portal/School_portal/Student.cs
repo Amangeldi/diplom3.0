63f67f686a056560921d2ea44b7acef0737c38a1
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-28 22:30:36 +0300

    Add class "Student"

---
+++ School_portal/School_portal/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_portal
+{
+    public class Student : Users
+    {
+        public int student_ticket_number { get; set; }
+        public int group_id { get; set; }
+        public Student(int user_id)
+        {
+            this.user_id = user_id;
+        }
+    }
+}
