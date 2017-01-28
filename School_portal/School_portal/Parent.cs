9eda79bb5a468465fcb089b74114e3858e6c54bc
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-28 22:31:59 +0300

    Add class "Parent"

---
+++ School_portal/School_portal/Parent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_portal
+{
+    public class Parent : Users
+    {
+        public int parent_id { get; set; }
+        public int student_ticket_number { get; set; }
+        public string tel { get; set; }
+        public Parent(int user_id)
+        {
+            this.user_id = user_id;
+        }
+    }
+}
