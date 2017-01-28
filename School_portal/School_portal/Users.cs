051dfb0464fd0d7153de8732ff607414e796f885
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-28 22:25:51 +0300

    Add class "Users"

---
+++ School_portal/School_portal/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_portal
+{
+    public class Users
+    {
+        public int user_id { get; set; }
+        public int role { get; set; }
+        public string familija { get; set; }
+        public string imja { get; set; }
+        public string otchestvo { get; set; }
+        public string adress { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string DOB { get; set; }
+    }
+}
