4de1ae606b144f049776ebf36e889cefa48437c5
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-28 22:33:44 +0300

    Add class "Groupp"

---
+++ School_portal/School_portal/Groupp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_portal
+{
+    public class Groupp
+    {
+        public int groupp_id { get; set; }
+        public string groupp_name { get; set; }
+        public int teacher_id { get; set; }
+    }
+}
