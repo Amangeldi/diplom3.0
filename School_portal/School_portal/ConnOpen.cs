2f2e15388490190e4710a582c9b96938bb988705
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-30 23:14:11 +0300

    create class for connection

---
+++ School_portal/School_portal/ConnOpen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace School_portal
+{
+    public class ConnOpen
+    {
+        static string connectionString = WebConfigurationManager.ConnectionStrings["school_portal"].ConnectionString;
+        public SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
