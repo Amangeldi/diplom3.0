740ad069fd0bd011239aa8b8013922b926c2c925
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-30 19:59:49 +0300

    Add page for test connection

---
+++ School_portal/School_portal/testConnection.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace School_portal
+{
+    public partial class testConnection : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = WebConfigurationManager.ConnectionStrings["school_portal"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            using (connection)
+            {
+                connection.Open();
+                Label1.Text = "<b>Версия сервера: </b>" + connection.ServerVersion;
+                Label1.Text += "<br /><b>Состояние соединения: </b> " + connection.State.ToString();
+            }
+            Label1.Text += "<br /><b>Состояние соединения после using: </b> " + connection.State.ToString();
+
+        }
+    }
+}
