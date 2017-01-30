afc3798114746d2ad9cede4f6b428d5efa9a4dde
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-30 22:09:16 +0300

    Edit Admin page

--- School_portal/School_portal/Admin.aspx
+++ School_portal/School_portal/Admin.aspx
@@ -9,6 +9,38 @@
 <body>
     <form id="form1" runat="server">
     <div>
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Список пользователей: "></asp:Label>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Добавить пользователя"></asp:Label>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Роль: "></asp:Label>
+        <asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem Value="1">Администратор</asp:ListItem>
+            <asp:ListItem Value="2">Преподаватель</asp:ListItem>
+            <asp:ListItem Value="3">Ученик</asp:ListItem>
+            <asp:ListItem Value="4">Родитель</asp:ListItem>
+        </asp:DropDownList>
+        <asp:Label ID="Label5" runat="server" Text=" Фамилия: "></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Label ID="Label6" runat="server" Text=" Имя: "></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:Label ID="Label7" runat="server" Text=" Отчество: "></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label8" runat="server" Text="Адрес"></asp:Label>
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <asp:Label ID="Label9" runat="server" Text=" Логин: "></asp:Label>
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <asp:Label ID="Label10" runat="server" Text=" Пароль "></asp:Label>
+        <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+        <asp:Label ID="Label11" runat="server" Text=" Дата рождения: "></asp:Label>
+        <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Добавить" />
+        <asp:Label ID="Label12" runat="server" Text="Label"></asp:Label>
+        <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Добавить группу" />
     
     </div>
     </form>
--- School_portal/School_portal/Admin.aspx.cs
+++ School_portal/School_portal/Admin.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,7 +11,70 @@ namespace School_portal
 {
     public partial class Admin1 : System.Web.UI.Page
     {
+        string role;
+        string role_text;
         protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = WebConfigurationManager.ConnectionStrings["school_portal"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            int id = Convert.ToInt32(Session["Value"]);
+            Label1.Text = id.ToString();
+            SqlCommand sqlCom = new SqlCommand("SELECT * FROM dbo.users WHERE user_id LIKE '%" + id + "'", connection);
+            SqlDataReader dr = sqlCom.ExecuteReader();
+            string FIO;
+            dr.Read();
+            FIO = dr["familija"].ToString() + " " + dr["imja"].ToString() + " " + dr["otchestvo"].ToString();
+            Label1.Text = "Здравствуйте " + FIO;
+            connection.Close();
+
+            //------------------------------------------------------
+
+            connection.Open();
+            string result = "";
+            SqlCommand command = new SqlCommand("SELECT * FROM dbo.users", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            result += "<table> <tr><td>id пользователя</td><td>роль</td> <td>фамилия</td><td>имя</td><td>отчество</td><td>адрес</td><td>login</td><td>Дата рождения</td> </tr>";
+            while (reader.Read())
+            {
+                role = reader["role"].ToString();
+                if (role == "1")
+                {
+                    role_text = "<b>Администратор</b>";
+                }
+                else if (role == "2")
+                {
+                    role_text = "<b>Преподаватель</b>";
+                }
+                else if (role == "3")
+                {
+                    role_text = "<b>Ученик</b>";
+                }
+                else if (role == "4")
+                {
+                    role_text = "<b>Родитель</b>";
+                }
+                result += "<tr> <td>" + reader["user_id"].ToString() + "</td>";
+                result += " <td>" + role_text + "</td>";
+                result += " <td>" + reader["familija"].ToString() + "</td>";
+                result += " <td>" + reader["imja"].ToString() + "</td>";
+                result += " <td>" + reader["otchestvo"].ToString() + "</td>";
+                result += " <td>" + reader["adress"].ToString() + "</td>";
+                result += " <td>" + reader["login"].ToString() + "</td>";
+                result += " <td>" + reader["DOB"].ToString() + "</td>";
+                result += "</tr>";
+            }
+            result += "</ table >";
+            Label2.Text += result;
+            reader.Close();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
         {
 
         }
