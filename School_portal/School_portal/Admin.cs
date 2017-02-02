84d9c5f8fcd4196cefdead89b573ddac187cc35f
Amangeldi <Amangeldi.9606@gmail.com>
2017-02-02 20:10:36 +0300

    Добавил функцию записи в таблицу Admin

--- School_portal/School_portal/Admin.cs
+++ School_portal/School_portal/Admin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -12,5 +13,21 @@ namespace School_portal
         {
             this.user_id = user_id;
         }
+        public ConnOpen add_admin = new ConnOpen();
+        public void add(int user_id)
+        {
+            //this.user_id = user_id;
+            add_admin.connection.Open();
+            string sql = string.Format("Insert Into admin" +
+                       "(user_id) Values(@user_id)");
+            using (SqlCommand cmd = new SqlCommand(sql, add_admin.connection))
+            {
+                cmd.Parameters.AddWithValue("@user_id", user_id);
+                cmd.ExecuteNonQuery();
+            }
+            add_admin.connection.Close();
+
+        }
+
     }
 }
