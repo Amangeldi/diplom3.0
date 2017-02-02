acb1cf2e74aecd480611302215e8da832215d424
Amangeldi <Amangeldi.9606@gmail.com>
2017-02-02 20:11:33 +0300

    Добавил функцию записи в таблицу Student

--- School_portal/School_portal/Student.cs
+++ School_portal/School_portal/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,21 @@ namespace School_portal
         public Student(int user_id)
         {
             this.user_id = user_id;
+        }
+        public ConnOpen add_student = new ConnOpen();
+        public void add (int user_id)
+        {
+            //this.user_id = user_id;
+            add_student.connection.Open();
+            string sql = string.Format("Insert Into student" +
+                       "(user_id, group_id) Values(@user_id, @group_id)");
+            using (SqlCommand cmd = new SqlCommand(sql, add_student.connection))
+            {
+                cmd.Parameters.AddWithValue("@user_id", user_id);
+                cmd.Parameters.AddWithValue("@group_id", 1);
+                cmd.ExecuteNonQuery();
+            }
+            add_student.connection.Close();
         }
     }
 }
