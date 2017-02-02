14ee8af2c5f9465dbc335067540474606392d186
Amangeldi <Amangeldi.9606@gmail.com>
2017-02-02 20:11:47 +0300

    Добавил функцию записи в таблицу Parent

--- School_portal/School_portal/Parent.cs
+++ School_portal/School_portal/Parent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -13,6 +14,22 @@ namespace School_portal
         public Parent(int user_id)
         {
             this.user_id = user_id;
+        }
+        public ConnOpen add_parent = new ConnOpen();
+        public void add(int user_id)
+        {
+            //this.user_id = user_id;
+            add_parent.connection.Open();
+            string sql = string.Format("Insert Into parent" +
+                       "(user_id, tel, student_ticket_number) Values(@user_id, @tel, @student_ticket_number)");
+            using (SqlCommand cmd = new SqlCommand(sql, add_parent.connection))
+            {
+                cmd.Parameters.AddWithValue("@user_id", user_id);
+                cmd.Parameters.AddWithValue("@tel", "+3752512345678");
+                cmd.Parameters.AddWithValue("@student_ticket_number", 1);
+                cmd.ExecuteNonQuery();
+            }
+            add_parent.connection.Close();
         }
     }
 }
