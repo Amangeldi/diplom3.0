119283cd7a677f189974675f7a5b2c242346b483
Amangeldi <Amangeldi.9606@gmail.com>
2017-02-04 06:48:46 +0300

    Последствия добавления label15

--- School_portal/School_portal/Admin.aspx.designer.cs
+++ School_portal/School_portal/Admin.aspx.designer.cs
@@ -202,6 +202,15 @@ namespace School_portal {
         protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
+        /// Label15 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label15;
+        
+        /// <summary>
         /// Label12 control.
         /// </summary>
         /// <remarks>
