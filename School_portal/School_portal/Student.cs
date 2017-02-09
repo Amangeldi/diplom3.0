991db7d222d64d6b525b363924052c5e23f4a931
Amangeldi <Amangeldi.9606@gmail.com>
2017-02-09 04:42:36 +0300

    Не рабочая версия. Удаляем студента и родителя

--- School_portal/School_portal/Student.cs
+++ School_portal/School_portal/Student.cs
@@ -15,6 +15,7 @@ namespace School_portal
             this.user_id = user_id;
         }
         public ConnOpen add_student = new ConnOpen();
+        public ConnOpen delete_student = new ConnOpen();
         public void add (int user_id)
         {
             //this.user_id = user_id;
@@ -28,6 +29,22 @@ namespace School_portal
                 cmd.ExecuteNonQuery();
             }
             add_student.connection.Close();
+        }
+        public new void delete(int user_id)
+        {
+            Parent parent = new Parent();
+            SqlCommand sqlCom = new SqlCommand("SELECT * FROM dbo.student WHERE user_id LIKE '%" + user_id + "'", delete_student.connection);
+            SqlDataReader dr = sqlCom.ExecuteReader();
+            dr.Read();
+            parent.delete(Convert.ToInt32(dr["student_ticket_number"]));
+            //==========
+            delete_student.connection.Close();
+            delete_student.connection.Open();
+            string sql = string.Format("Delete from student where user_id = '{0}'", user_id);
+            using (SqlCommand cmd = new SqlCommand(sql, delete_student.connection))
+            {
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
