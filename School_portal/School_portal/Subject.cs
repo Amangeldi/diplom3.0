d61b15d2a2a63917d835ca675f1d2ef9972a5934
Amangeldi <Amangeldi.9606@gmail.com>
2017-04-05 20:46:38 +0300

    Создал нужные классы

---
+++ School_portal/School_portal/Subject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_portal
+{
+    public class Subject
+    {
+        public int subject_id { get; set; }
+
+    }
+}
---
+++ School_portal/School_portal/Timetable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_portal
+{
+    public partial class Timetable
+    {
+        public DateTime data { get; set; }
+        public int subject_id { get; set; }
+        public int teacher_id { get; set; }
+        public int group_id { get; set; }
+    }
+}
