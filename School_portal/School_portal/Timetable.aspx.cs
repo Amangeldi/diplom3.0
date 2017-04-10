b61ffb548f6c79b3cdf7e203cee8bb769f300419
Amangeldi <Amangeldi.9606@gmail.com>
2017-04-10 15:09:02 +0300

    Добавил возможность выбора предмета для расписании

--- School_portal/School_portal/Timetable.aspx.cs
+++ School_portal/School_portal/Timetable.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +12,7 @@ namespace School_portal
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string sName = "";
             ConnOpen tLoad = new ConnOpen();
             if (Session["Value"] != null)
             {
@@ -19,6 +21,18 @@ namespace School_portal
                 if(r!=1)
                 {
                     Panel1.Enabled = false;
+                }
+                else
+                {
+                    tLoad.connection.Open();
+                    SqlCommand command_subject = new SqlCommand("SELECT * FROM dbo.subject", tLoad.connection);
+                    SqlDataReader reader_subject = command_subject.ExecuteReader();
+                    while(reader_subject.Read())
+                    {
+                        sName = reader_subject["subject_name"].ToString();
+                        DropDownList1.Items.Add(new ListItem(sName, reader_subject["subject_id"].ToString()));
+                    }
+                    tLoad.connection.Close();
                 }
             }
             
